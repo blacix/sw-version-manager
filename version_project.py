13784377cc5e0398029fb26544f576140a0eaf54
Laszlo Bartok <bartok@nowtech.hu>
2022-06-25 17:18:20 +0200

    config template

--- version_project.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./scripts/')
-
-PROJECT_VERSION_TAGS = ['APP_VERSION_MAJOR', 'APP_VERSION_MINOR', 'APP_VERSION_PATCH']
-GIT_TAG_PREFIX = 'V'
-
-if __name__ == '__main__':
-    import version
-    sys.exit(version.update_versions(sys.argv, GIT_TAG_PREFIX, PROJECT_VERSION_TAGS))
