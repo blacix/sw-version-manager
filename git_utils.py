7cf99b47510a69431b32e6bd3e98530418836141
Laszlo Bartok <blacix@hotmail.com>
2024-03-06 16:15:46 +0100

    main method for git utils

--- git_utils.py
+++ git_utils.py
@@ -1,4 +1,5 @@
 import git
+import argparse
 
 
 class GitUtils:
@@ -14,12 +15,43 @@ class GitUtils:
 
     def commit_file(self, file_path: str, commit_message: str):
         if self.repo.is_dirty(path=file_path):
-            print(f'commit_file: {file_path} {commit_message}')
+            # print(f'commit_file: {file_path} {commit_message}')
             self.repo.index.add(file_path)
             self.repo.index.commit(commit_message)
             self.repo.git.push('origin', self.repo.active_branch.name)
 
     def create_tag(self, tag_name):
-        print(f'tag: {tag_name}')
+        # print(f'tag: {tag_name}')
         self.repo.create_tag(tag_name, message=tag_name)
         self.repo.git.push('origin', tag_name)
+
+
+def bash_exit(result: bool):
+    if result:
+        print('true')
+        exit(0)
+    else:
+        print('false')
+        exit(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Command-line arguments example')
+    parser.add_argument('--repo', default=".", help='Path to the git repository')
+    parser.add_argument('--tag', required=True, help='Name of the git tag')
+    parser.add_argument('--check', action='store_true', help='checks if the tag provided is on the current commit')
+    parser.add_argument('--create', action='store_true', help='creates tag on the current commit')
+    args = parser.parse_args()
+    gitUtils = GitUtils(args.repo)
+    if args.create:
+        try:
+            gitUtils.create_tag(args.tag)
+            bash_exit(True)
+        except Exception as e:
+            print(e)
+            bash_exit(False)
+
+    if args.check:
+        bash_exit(gitUtils.check_tag_on_current_commit(args.tag))
+
+    exit(0)
