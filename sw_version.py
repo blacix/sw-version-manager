e3a606b85f55a7e458f48367d680af91203ad3e9
Laszlo Bartok <blacix@hotmail.com>
2024-03-13 15:57:36 +0100

    separate methods for version bump program arguments

--- sw_version.py
+++ sw_version.py
@@ -12,24 +12,27 @@ from parsers.string_file_parser import StringFileParser
 class SoftwareVersion:
     VALID_BUMPS = [Common.TAG_MAJOR, Common.TAG_MINOR, Common.TAG_PATCH, Common.TAG_PRE_RELEASE, Common.TAG_BUILD]
 
-    def __init__(self):
-        args = self._parse_arguments()
+    def __init__(self, language, version_file, to_bump=None, commit=False, create_git_tag=False, git_tag_prefix=None,
+                 check_git_tag=False,
+                 pre_release_prefix=None, build_prefix=None, repo_path='.'):
 
-        self.language: str = args.lang
-        self.version_file: str = args.file
-        self.bump: str = args.bump
-        self.commit: bool = args.commit
-        self.create_git_tag: bool = args.tag
-        self.git_tag_prefix = args.tag_prefix
+        self.language: str = language
+        self.version_file: str = version_file
+        self.to_bump: str = to_bump
+        self.commit: bool = commit
+        self.create_git_tag: bool = create_git_tag
+        self.git_tag_prefix = git_tag_prefix
         self.git_tag = self.git_tag_prefix
-        self.check_git_tag = args.check_tag
-        self.pre_release_prefix = args.pre_release_prefix
-        self.build_prefix = args.build_prefix
-        self.repo_path = args.repo
+        self.check_git_tag = check_git_tag
+        self.pre_release_prefix = pre_release_prefix
+        self.build_prefix = build_prefix
+        self.repo_path = repo_path
 
         self.version: semver.Version = None
 
         self.git: GitUtils = None
+        if (self.commit or self.create_git_tag or self.check_git_tag) and self.git is None:
+            self.git = GitUtils(self.repo_path)
 
         if self.language in TagFileParser.LANGUAGES:
             self.parser = TagFileParser(self.language, self.version_file, self.pre_release_prefix, self.build_prefix)
@@ -41,7 +44,10 @@ class SoftwareVersion:
             self.parser: VersionFileParser = VersionFileParser()
             print(f'no parser for language: {self.language}')
 
-    def _parse_arguments(self):
+        self._parse()
+
+    @staticmethod
+    def parse_arguments():
         parser = argparse.ArgumentParser(description='Command-line arguments example')
         # Mandatory arguments
         parser.add_argument('--file', required=True, help='Specify the filename')
@@ -52,7 +58,7 @@ class SoftwareVersion:
         parser.add_argument('--commit', action='store_true', help='commit the updated version file')
         parser.add_argument('--tag', action='store_true', help='add a tag with the version to git')
         parser.add_argument('--tag_prefix', default='', help='Prefix for git tag')
-        parser.add_argument('--bump', choices=self.VALID_BUMPS, help='Specify the tag to bump')
+        parser.add_argument('--bump', choices=SoftwareVersion.VALID_BUMPS, help='Specify the tag to bump')
         parser.add_argument('--check_tag', action='store_true',
                             help='checks is the tag is already on the current commit')
         parser.add_argument('--pre_release_prefix', default=None, help='The prefix for the pre-release number part')
@@ -60,21 +66,32 @@ class SoftwareVersion:
         parser.add_argument('--repo', default=".", help='Path to the git repository')
         return parser.parse_args()
 
-    def _bump(self):
+    def _parse(self):
+        semver.VersionInfo.major
+        self.version = self.parser.parse()
+        # When a mandatory version is bumped with semver, the optional parts with the value 0
+        # are emitted from the version object.
+        # When a string contains the optional 0 parts, and it is parsed with semver, the version object will contain
+        # the optional 0 parts.
+        # This can be an issue when comparing git tags when we don't bump any version, just parse a string with
+        # optional parts being 0, e.g. we can have a tag: 0.0.0-0+0
+        self.version = Common.emit_optional_zero_parts(self.version)
+
+    def _bump_version(self, bump):
         if self.version is None:
             return
-        elif Common.TAG_MAJOR.lower() == self.bump.lower():
+        elif Common.TAG_MAJOR.lower() == bump.lower():
             self.version = self.version.bump_major()
-        elif Common.TAG_MINOR.lower() == self.bump.lower():
+        elif Common.TAG_MINOR.lower() == bump.lower():
             self.version = self.version.bump_minor()
-        elif Common.TAG_PATCH.lower() == self.bump.lower():
+        elif Common.TAG_PATCH.lower() == bump.lower():
             self.version = self.version.bump_patch()
-        elif Common.TAG_PRE_RELEASE.lower() == self.bump.lower():
+        elif Common.TAG_PRE_RELEASE.lower() == bump.lower():
             if self.pre_release_prefix is not None:
                 self.version = self.version.bump_prerelease(self.pre_release_prefix)
             else:
                 self.version = self.version.bump_prerelease()
-        elif Common.TAG_BUILD.lower() in self.bump.lower():
+        elif Common.TAG_BUILD.lower() in bump.lower():
             if self.build_prefix is not None:
                 self.version = self.version.bump_build(self.build_prefix)
             else:
@@ -84,35 +101,14 @@ class SoftwareVersion:
             pass
 
     def execute(self) -> int:
-        self.version = self.parser.parse()
         if self.version is None:
             print('Parse error')
             return 1
 
         return_value = 0
-        if (self.commit or self.create_git_tag or self.check_git_tag) and self.git is None:
-            self.git = GitUtils(self.repo_path)
 
         if self.check_git_tag:
-            pre_bump_git_tag = self.git_tag_prefix + str(self.version)
-            if self.git.check_tag_on_current_commit(pre_bump_git_tag):
-                print(f'tag on current commit: {pre_bump_git_tag}')
-                self.bump = False
-                self.create_git_tag = False
-                self.commit = False
-                return_value = 2
-
-        if self.bump:
-            self._bump()
-            self.parser.update(self.version)
-        else:
-            # When a mandatory version is bumped with semver, the optional parts with the value 0
-            # are emitted from the version object.
-            # When a string contains the optional 0 parts, and it is parsed with semver, the version object will contain
-            # the optional 0 parts.
-            # This can be an issue when comparing git tags when we don't bump any version, just parse a string with
-            # optional parts being 0, e.g. we can have a tag: 0.0.0-0+0
-            self.version = Common.emit_optional_zero_parts(self.version)
+            return_value = self._check_git_tag()
 
         self.git_tag = self.git_tag_prefix + str(self.version)
         if self.commit:
@@ -122,9 +118,12 @@ class SoftwareVersion:
                 print(e)
                 return_value = 3
 
+        if self.to_bump:
+            self.bump(self.to_bump)
+
         if self.create_git_tag:
             try:
-                self.git.create_tag(self.git_tag)
+                self.git.tag_repo(self.git_tag)
             except Exception as e:
                 print(e)
                 return_value = 4
@@ -133,6 +132,24 @@ class SoftwareVersion:
         print(result)
         return return_value
 
+    def _check_git_tag(self):
+        return_value = 0
+        pre_bump_git_tag = self.git_tag_prefix + str(self.version)
+        if self.git.check_tag_on_current_commit(pre_bump_git_tag):
+            print(f'tag on current commit: {pre_bump_git_tag}')
+            self.to_bump = False
+            self.create_git_tag = False
+            self.commit = False
+            return_value = 2
+        return return_value
+
+    def bump(self, bump):
+        self._bump_version(bump)
+        self.parser.update(self.version)
+        return self.version
+
 
 if __name__ == '__main__':
-    sys.exit(SoftwareVersion().execute())
+    args = SoftwareVersion.parse_arguments()
+    sys.exit(SoftwareVersion(args.lang, args.file, args.bump, args.commit, args.tag, args.tag_prefix, args.check_tag,
+                             args.pre_release_prefix, args.build_prefix, args.repo).execute())
